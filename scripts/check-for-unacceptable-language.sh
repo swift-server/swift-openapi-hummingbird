6465ae42c85f7dfb9d59d579f0ee05e0f9ef1128
Adam Fowler <adamfowler71@gmail.com>
2023-11-29 12:17:42 +0000

    Update file headers

--- scripts/check-for-unacceptable-language.sh
+++ scripts/check-for-unacceptable-language.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 ##===----------------------------------------------------------------------===##
 ##
+## This source file is part of the Hummingbird server framework project
+##
+## Copyright (c) 2023 the Hummingbird authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of Hummingbird authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+##===----------------------------------------------------------------------===##
+##
 ## This source file is part of the SwiftOpenAPIGenerator open source project
 ##
 ## Copyright (c) 2023 Apple Inc. and the SwiftOpenAPIGenerator project authors
--- scripts/generate-contributors-list.sh
+++ scripts/generate-contributors-list.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 ##===----------------------------------------------------------------------===##
 ##
-## This source file is part of the SwiftOpenAPIGenerator open source project
+## This source file is part of the Hummingbird server framework project
 ##
-## Copyright (c) 2023 Apple Inc. and the SwiftOpenAPIGenerator project authors
+## Copyright (c) 2023 the Hummingbird authors
 ## Licensed under Apache License v2.0
 ##
 ## See LICENSE.txt for license information
-## See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
+## See CONTRIBUTORS.txt for the list of Hummingbird authors
 ##
 ## SPDX-License-Identifier: Apache-2.0
 ##
